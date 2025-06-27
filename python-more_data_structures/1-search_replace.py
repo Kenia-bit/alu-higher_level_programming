839638a903ba6dd2b517a1f145189b4525bc12cb
kenia-bit <u.kenia@alustudent.com>
2025-06-27 21:03:38 +0200

    1-search_replace.py

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+
+
 # function that replaces all occurrences of an element by another in a new lis
 def search_replace(my_list, search, replace):
     new_list = list(map(lambda x: replace if x == search else x, my_list))
