16017546b9c3ed4b5d00c47e348b7b73f22eba93
kenia-bit <u.kenia@alustudent.com>
2025-06-27 20:44:18 +0200

    8-uppercase.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 def uppercase(str):
-    strlen = len(str)
-    if strlen == 0:
-        print()  # Handle the empty string case
-        return
+    str = str if isinstance(str, str) else str.__str__()
+    result = ""
 
-    i = 0
-    while i < strlen:
-        char = ord(str[i])
-        if char < 97 or char > 122:
-            print("{}".format(chr(char)), end="" if i < strlen - 1 else "\n")
+    for char in str:
+        if 'a' <= char <= 'z':
+            result += chr(ord(char) - 32)
         else:
-            char = char - 32
-            print("{}".format(chr(char)), end="" if i < strlen - 1 else "\n")
-        i += 1
+            result += char
+
+    print("{}".format(result))
