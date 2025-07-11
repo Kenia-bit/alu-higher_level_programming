cfb274f19ae2bffbea8992022cc06e323c1340e3
Kenia Umutoni <u.kenia@alustudent.com>
2025-07-11 23:18:45 +0200

    Update 1-main.py

--- python-inheritance/tests/1-main.py
+++ python-inheritance/tests/1-main.py
@@ -1,14 +1,30 @@
-#!/usr/bin/python3
-MyList = __import__('1-my_list').MyList
+>>> MyList = __import__('1-my_list').MyList
 
-my_list = MyList()
-my_list.append(1)
-my_list.append(4)
-my_list.append(2)
-my_list.append(3)
-my_list.append(5)
+>>> new_list = MyList()
 
-print("Original list:", my_list)
-sorted_version = my_list.print_sorted()
-print("Returned sorted list:", sorted_version)
-print("Original list after sorting:", my_list)
+>>> new_list
+[]
+
+>>> new_list.print_sorted()
+[]
+
+>>> new_list.append(4)
+
+>>> new_list
+[4]
+
+>>> new_list.append(2)
+
+>>> new_list
+[4, 2]
+
+>>> new_list.append(-1)
+
+>>> new_list
+[4, 2, -1]
+
+>>> new_list.print_sorted()
+[-1, 2, 4]
+
+>>> new_list
+[4, 2, -1]
