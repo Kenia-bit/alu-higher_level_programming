2a7b149074c5f460cfcbc5b7dade374ad299af9a
kenia-bit <u.kenia@alustudent.com>
2025-07-21 15:23:53 +0200

    Commit deleted files before rebase

--- python-inheritance/5-base_geometry.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""creates an empty class"""
-
-
-class BaseGeometry:
-    """created an empty class to be used in future"""
-    pass
--- python-inheritance/6-base_geometry.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""creates a class that raises an exception"""
-
-
-class BaseGeometry:
-    """this class raises an exception"""
-    def area(self):
-        raise Exception("area() is not implemented")
