bf3f1644ef4c2d5a5eb2382c224621df2578c7eb
kenia-bit <u.kenia@alustudent.com>
2025-07-21 16:13:18 +0200

    12-pascal_triangle.py

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,17 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]  # First row
+
+    for i in range(1, n):
+        row = [1]  # First element
+        prev_row = triangle[i - 1]
+
+        for j in range(1, i):
+            row.append(prev_row[j - 1] + prev_row[j])  # Middle elements
+
+        row.append(1)  # Last element
+        triangle.append(row)
+
+    return triangle
