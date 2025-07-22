40e86745ccc66b67f780b789c964e7fe4ecac6b4
kenia-bit <u.kenia@alustudent.com>
2025-07-22 23:08:26 +0200

    4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-"""Fetches http://0.0.0.0:5050/status using requests"""
+"""Fetches https://intranet.hbtn.io/status using requests"""
 
 import requests
 
 if __name__ == "__main__":
-    url = "http://0.0.0.0:5050/status"
-    response = requests.get(url)
-    content = response.text
-
-    print("Body response:")
-    print("\t- type: {}".format(type(content)))
-    print("\t- content: {}".format(content))
+    url = "https://intranet.hbtn.io/status"
+    headers = {'User-Agent': 'Mozilla/5.0 (compatible)'}
+    try:
+        response = requests.get(url, headers=headers, verify=False)
+        content = response.text
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+    except requests.exceptions.RequestException as e:
+        print("Error:", e)
