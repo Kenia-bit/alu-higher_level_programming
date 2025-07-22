1225694e3a98ebad4d70176222b1a3b36a2115bd
kenia-bit <u.kenia@alustudent.com>
2025-07-22 14:17:25 +0200

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-"""A script that opens a TCP connection to a url and prints its content"""
+"""A script that opens a TCP connection to a URL and prints its content"""
 
 from urllib.request import urlopen
+import sys
+
 if __name__ == "__main__":
-    with urlopen("https://alx-intranet.hbtn.io/status") as response:
-        response = response.read()
+    url = sys.argv[1] if len(sys.argv) > 1 else "https://alx-intranet.hbtn.io/status"
+    with urlopen(url) as response:
+        content = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(response)))
-        print("\t- content: {}".format(response))
-        print("\t- utf8 content: {}".format(response.decode("utf-8")))
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
