531da09ac9404f9d4846d4905315912b1d9e3855
kenia-bit <u.kenia@alustudent.com>
2025-07-22 13:48:48 +0200

    0-body_size.sh

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Starts Flask server in background, waits, and fetches index page
-python3 web_0.py & sleep 1 && echo "GET / => \"$(curl -s http://127.0.0.1:5000/)\""
+# Fetches the body of a web page and prints it with GET / => prefix
+echo "GET / => \"$(curl -s "$1")\""
