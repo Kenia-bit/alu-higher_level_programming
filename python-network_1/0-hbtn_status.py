1fbf34070cc6f0cd639b2583992d7149bb8dc33e
kenia-bit <u.kenia@alustudent.com>
2025-07-22 15:33:22 +0200

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -5,10 +5,7 @@ from urllib.request import Request, urlopen
 
 if __name__ == "__main__":
     url = "https://intranet.hbtn.io/status"
-    req = Request(url, headers={
-        "User-Agent": "Mozilla/5.0"
-    })
-
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
     with urlopen(req) as response:
         body = response.read()
         print("Body response:")
