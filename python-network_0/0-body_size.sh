9fe5801cab32d6819dc0cc38edaeb1b4857f77e6
kenia-bit <u.kenia@alustudent.com>
2025-07-22 13:42:13 +0200

    0-body_size.sh

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+"""Starts a basic web server with a single route"""
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def index():
-    return "my index page"
+    return "my index page is so cool! Mainly because it’s in Python"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
