a04dc3c5282ffc645cdd0e3dbd406c72299a6b37
kenia-bit <u.kenia@alustudent.com>
2025-07-29 12:43:21 +0200

    4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-"""Fetches the status from a URL using requests and displays the response body."""
+"""Fetches the status from a given URL and prints the response content."""
 
 import requests
 
 
-if __name__ == "__main__":
-    url = "http://0.0.0.0:5050/status"
-    # url = "https://intranet.hbtn.io/status"
-
+def fetch_status(url):
+    """Fetch and display the response body from a given URL."""
     response = requests.get(url)
     content = response.text
 
     print("Body response:")
     print(f"\t- type: {type(content)}")
     print(f"\t- content: {content}")
+
+
+if __name__ == "__main__":
+    fetch_status("http://0.0.0.0:5050/status")
+    # To test intranet, use this instead:
+    # fetch_status("https://intranet.hbtn.io/status")
