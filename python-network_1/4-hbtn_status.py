4049c9c3886c24818dd033c35dd862a25c128e49
kenia-bit <u.kenia@alustudent.com>
2025-07-29 13:08:23 +0200

    4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-"""Fetches status from a local web server using requests"""
+"""Fetches status from a given URL using the requests module"""
 
 import requests
 
 if __name__ == "__main__":
-    url = "http://0.0.0.0:5050/status"
-    response = requests.get(url)
-    content = response.text
+    url = "http://0.0.0.0:5050/status"  # Change to https://alu-intranet.hbtn.io/status when testing remotely
 
-    print("Body response:")
-    print("\t- type: {}".format(type(content)))
-    print("\t- content: {}".format(content))
+    try:
+        response = requests.get(url)
+        content = response.text
+
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+    except Exception as e:
+        print("Error: {}".format(e))
