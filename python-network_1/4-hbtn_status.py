9711e206f942c752b1cfd756643e783341e27d7d
kenia-bit <u.kenia@alustudent.com>
2025-07-29 12:32:22 +0200

    4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -2,12 +2,12 @@
 import requests
 
 if __name__ == "__main__":
-    url = "http://0.0.0.0:5050/status"  # or https://intranet.hbtn.io/status
+    url = "http://0.0.0.0:5050/status"  # Or https://intranet.hbtn.io/status
     try:
         response = requests.get(url)
         content = response.text
         print("Body response:")
         print(f"\t- type: {type(content)}")
         print(f"\t- content: {content}")
-    except Exception as e:
-        print("Error:", e)
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
