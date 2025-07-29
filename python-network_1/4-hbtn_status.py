7a076976c5cb57e9c4f0cd433430c3097f437a6a
kenia-bit <u.kenia@alustudent.com>
2025-07-29 13:28:08 +0200

    4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -4,22 +4,22 @@
 import requests
 
 
-def fetch_url(url):
-    """Fetch a URL and print its response content formatted."""
+def fetch_status(url):
+    """Fetch URL content and print formatted output."""
     try:
         response = requests.get(url)
         response.raise_for_status()
         print("Body response:")
         print(f"\t- type: {type(response.text)}")
         print(f"\t- content: {response.text}")
-        return True
     except requests.RequestException:
         return False
+    return True
 
 
 if __name__ == "__main__":
     local_url = "http://0.0.0.0:5050/status"
     remote_url = "https://intranet.hbtn.io/status"
 
-    if not fetch_url(local_url):
-        fetch_url(remote_url)
+    if not fetch_status(local_url):
+        fetch_status(remote_url)
