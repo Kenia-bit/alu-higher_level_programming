9bffe2adfedca52a005d113b4118dc842b146487
kenia-bit <u.kenia@alustudent.com>
2025-07-29 12:56:55 +0200

    4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-"""Fetches custom status from a local test server using requests"""
+"""Fetches the status from a URL using requests and displays the response body."""
 
 import requests
 
+
 if __name__ == "__main__":
-    url = "http://0.0.0.0:5050/status"  # <-- Test server URL
+    url = "http://0.0.0.0:5050/status"
+    # url = "https://intranet.hbtn.io/status"
+
     response = requests.get(url)
     content = response.text
 
     print("Body response:")
-    print("\t- type: {}".format(type(content)))
-    print("\t- content: {}".format(content))
+    print(f"\t- type: {type(content)}")
+    print(f"\t- content: {content}")
