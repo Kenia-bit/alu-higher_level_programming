7ab72c89233548b3859fc0326486efc58bb499b5
kenia-bit <u.kenia@alustudent.com>
2025-07-29 11:14:15 +0200

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-"""Fetches a URL and prints the response body with formatting."""
+"""Fetch https://alu-intranet.hbtn.io/status and display the body."""
 
 from urllib import request
 
-url = "http://0.0.0.0:5050/status"  # Change this line when testing on intranet
-
-try:
+if __name__ == "__main__":
+    url = "https://alu-intranet.hbtn.io/status"
     with request.urlopen(url) as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
         print("\t- utf8 content: {}".format(body.decode("utf-8")))
-except Exception as e:
-    print("Error:", e)
