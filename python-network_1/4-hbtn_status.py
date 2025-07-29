5b58f8feecbee687905f29ea6c669d37655a1bff
kenia-bit <u.kenia@alustudent.com>
2025-07-29 11:35:19 +0200

    0-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
-"""Fetches https://alu-intranet.hbtn.io/status using requests"""
+"""Fetches the status page and prints the response body."""
 
 import requests
-
+import sys
 
 if __name__ == "__main__":
-    url = "https://alu-intranet.hbtn.io/status"
+    # Default URL if none given
+    url = "https://intranet.hbtn.io/status"
+    if len(sys.argv) == 2:
+        url = sys.argv[1]
+
     try:
         response = requests.get(url)
+        response.raise_for_status()
         content = response.text
         print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
+        print(f"\t- type: {type(content)}")
+        print(f"\t- content: {content}")
     except requests.exceptions.RequestException as e:
         print("Error:", e)
