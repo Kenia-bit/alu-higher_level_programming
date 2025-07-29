a1cc929140737a012a622ee28c92016381143739
kenia-bit <u.kenia@alustudent.com>
2025-07-29 11:10:51 +0200

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-"""Fetches a URL and prints formatted response"""
+"""Fetches a URL and prints the response body with formatting."""
 
-from urllib.request import urlopen
-import sys
+from urllib import request
 
-if __name__ == "__main__":
-    url = sys.argv[1] if len(sys.argv) > 1 else "https://alu-intranet.hbtn.io/status"
-    with urlopen(url) as response:
+url = "http://0.0.0.0:5050/status"  # Change this line when testing on intranet
+
+try:
+    with request.urlopen(url) as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
         print("\t- utf8 content: {}".format(body.decode("utf-8")))
+except Exception as e:
+    print("Error:", e)
