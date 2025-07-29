e35e086f998c13bc0e51ea6f381f767b56f0c649
kenia-bit <u.kenia@alustudent.com>
2025-07-29 10:49:48 +0200

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,9 +2,10 @@
 """Fetches a URL and prints formatted response"""
 
 from urllib.request import urlopen
+import sys
 
 if __name__ == "__main__":
-    url = "http://0.0.0.0:5050/status"  # This is what the checker uses
+    url = sys.argv[1] if len(sys.argv) > 1 else "https://alu-intranet.hbtn.io/status"
     with urlopen(url) as response:
         body = response.read()
         print("Body response:")
