a13b92a58edc7031e76423aed6a0929cd15e0854
kenia-bit <u.kenia@alustudent.com>
2025-07-29 11:40:04 +0200

    0-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-"""Fetches a URL using requests and prints the response"""
+"""Fetches https://intranet.hbtn.io/status using only requests"""
 
 import requests
 
 
 if __name__ == "__main__":
     url = "https://intranet.hbtn.io/status"
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 '
+                      '(KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'
+    }
     try:
-        response = requests.get(url)
+        response = requests.get(url, headers=headers)
         content = response.text
         print("Body response:")
         print("\t- type: {}".format(type(content)))
