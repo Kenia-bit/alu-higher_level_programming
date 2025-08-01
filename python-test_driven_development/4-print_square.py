d5b838e7b16a41c4cfe435923469ec18b28aa8f2
kenia-bit <u.kenia@alustudent.com>
2025-08-01 22:36:20 +0200

    4-print_square.py

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
 """
-This module defines a function to print a square with the character '#'
+This module defines a function that prints a square using '#' characters.
 """
 
 
 def print_square(size):
-    """Prints a square of '#' of size `size`
+    """Prints a square with the character '#'
 
     Args:
-        size (int): The size length of the square
+        size (int): The size of the square
 
     Raises:
         TypeError: If size is not an integer
-        ValueError: If size is negative
+        ValueError: If size is less than 0
     """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
-
     if size < 0:
         raise ValueError("size must be >= 0")
 
