cf6ba2d77c8ce275e6c2fb505cd2c63a8329ac90
kenia-bit <u.kenia@alustudent.com>
2025-08-01 21:59:32 +0200

    4-print_square.py

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,27 +1,5 @@
 #!/usr/bin/python3
-"""
-Module that defines a function to print a square made of '#' characters.
-"""
-
-
-def print_square(size):
-    """
-    Prints a square of '#' characters with the given size.
-
-    Args:
-        size (int): The length of each side of the square.
-
-    Raises:
-        TypeError: If size is not an integer.
-        ValueError: If size is less than 0.
-    """
-    if not isinstance(size, int) or isinstance(size, bool):
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
-
-    for _ in range(size):
-        print("#" * size)
+#!/usr/bin/python3
 """
 >>> print_square(4)
 ####
@@ -49,3 +27,30 @@ Traceback (most recent call last):
 ...
 TypeError: size must be an integer
 """
+
+def print_square(size):
+    # your function code here
+
+"""
+Module that defines a function to print a square made of '#' characters.
+"""
+
+
+def print_square(size):
+    """
+    Prints a square of '#' characters with the given size.
+
+    Args:
+        size (int): The length of each side of the square.
+
+    Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is less than 0.
+    """
+    if not isinstance(size, int) or isinstance(size, bool):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for _ in range(size):
+        print("#" * size)
