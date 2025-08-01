f4ec095549947d7c6fe8d55de233018222d86a5b
kenia-bit <u.kenia@alustudent.com>
2025-08-01 21:29:06 +0200

    5-text_indetation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -2,15 +2,22 @@
 def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
+
     i = 0
     length = len(text)
+
     while i < length:
         print(text[i], end="")
         if text[i] in ['.', '?', ':']:
-            print("\n")
-            # Skip any spaces after punctuation
+            print("\n")  # print two new lines after punctuation
+
+            # skip spaces following punctuation
             i += 1
             while i < length and text[i] == ' ':
                 i += 1
+            
+            # avoid extra blank line at the end
+            if i >= length:
+                break
             continue
         i += 1
