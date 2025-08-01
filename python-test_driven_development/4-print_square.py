afbbae0891a233245426bb2186f405ee9fee3af9
kenia-bit <u.kenia@alustudent.com>
2025-08-01 22:01:30 +0200

    4-print_square.py

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,36 +1,4 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
-"""
->>> print_square(4)
-####
-####
-####
-####
-
->>> print_square(1)
-#
-
->>> print_square(0)
-
->>> print_square(-1)
-Traceback (most recent call last):
-...
-ValueError: size must be >= 0
-
->>> print_square(3.5)
-Traceback (most recent call last):
-...
-TypeError: size must be an integer
-
->>> print_square("3")
-Traceback (most recent call last):
-...
-TypeError: size must be an integer
-"""
-
-def print_square(size):
-    # your function code here
-
 """
 Module that defines a function to print a square made of '#' characters.
 """
